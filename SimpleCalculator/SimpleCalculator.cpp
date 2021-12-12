66a0df6d831d8b99a211fdb16c2e226aa34aca26
Evgeniy <moytlf@gmail.com>
2021-12-12 13:38:20 +0300

    Debugging information has been removed

--- SimpleCalculator/SimpleCalculator.cpp
+++ SimpleCalculator/SimpleCalculator.cpp
@@ -1,11 +1,5 @@
-﻿#include <iostream>
-#include "SyntaxAnalyzer.h"
-#include "RPNGenerator.h"
-#include <string>
-
+﻿
 int main()
 {
-	RPNGenerator generator;
-	std::cout << generator.generate("5*5*5+(5*6+3)+7*3");
 	return 0;
 }
